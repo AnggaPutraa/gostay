e67c5d49910a28dd24eb5ef096976cea5b66d580
AnggaPutraa <yananggaa@gmail.com>
2023-04-07 22:45:24 +0700

    refactor: styles navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,8 @@
+'use client';
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
@@ -8,6 +11,8 @@ const Navbar = () => {
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
                         <Logo />
+                        <Search />
+                        <UserMenu />
                     </div>
                 </Container>
             </div>
