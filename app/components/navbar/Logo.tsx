40e175f3d041773a053c4a60382b7ad0b3c77a48
AnggaPutraa <yananggaa@gmail.com>
2023-04-08 20:35:37 +0700

    refactor: redirect logo click to main page

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,8 +7,9 @@ const Logo = () => {
 
     return (
         <Image
+            onClick={() => router.push('/')}
             alt='gostay'
-            className='hidden md:block'
+            className='hidden md:block cursor-pointer'
             height={100}
             width={100}
             src='/images/gostay.png'
