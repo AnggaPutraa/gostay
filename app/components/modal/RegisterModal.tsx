a7e8eec8374baa079116c2827c7f9b6a61815768
AnggaPutraa <yananggaa@gmail.com>
2023-04-08 15:15:23 +0700

    refactor: close modals

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -10,6 +10,7 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
+import Button from '../Button';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -43,7 +44,7 @@ const RegisterModal = () => {
             });
     }
 
-    const content = (
+    const bodyContent = (
         <div className='flex flex-col gap-4'>
             <Heading
                 title='Welcome to gostay'
@@ -77,6 +78,36 @@ const RegisterModal = () => {
         </div>
     );
 
+    const footerContent = (
+        <div className='flex flex-col gap-4 mt-3'>
+            <hr />
+            <Button
+                outline
+                label='Continue with Google'
+                icon={FcGoogle}
+                onClick={() => { }}
+            />
+            <Button
+                outline
+                label='Continue with Github'
+                icon={AiFillGithub}
+                onClick={() => { }}
+            />
+            <div className='text-neutral-500 text-center mt-2 font-light'>
+                <div className='flex flex-row items-center justify-center gap-2'>
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div
+                        onClick={registerModal.onClose}
+                        className='text-neutral-800 cursor-pointer hover:underline'>
+                        Sign In
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <Modal
             disabled={isLoading}
@@ -85,7 +116,8 @@ const RegisterModal = () => {
             actionLabel='Continue'
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
-            body={content}
+            body={bodyContent}
+            footer={footerContent}
         />
     );
 }
