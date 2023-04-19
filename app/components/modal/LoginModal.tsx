94ef1a6765faa1ca81c876b45d19e1a58d0bc6b3
AnggaPutraa <yananggaa@gmail.com>
2023-04-19 14:45:37 +0700

    refactor: remove unused text

--- app/components/modal/LoginModal.tsx
+++ app/components/modal/LoginModal.tsx
@@ -4,15 +4,17 @@ import { signIn } from 'next-auth/react';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from 'react-hot-toast';
+
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import Button from '../Button';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { useRouter } from 'next/navigation';
+
 
 const LoginModal = () => {
     const registerModal = useRegisterModal();
