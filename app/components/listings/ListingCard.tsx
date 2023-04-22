9c8b3ff620c1f700acd030b802b0a54a5485b6f0
AnggaPutraa <yananggaa@gmail.com>
2023-04-22 21:40:52 +0700

    refactor: listing card

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,10 +1,16 @@
+'use client';
 
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { format } from 'date-fns';
+import React, { useCallback, useMemo } from "react";
+
+import useCountries from "@/app/hooks/useCountry";
 
 interface ListingCardProps {
     data: Listing;
-    reservarion?: Reservation;
+    reservation?: Reservation;
     onAction?: (id: string) => void
     disabled?: boolean;
     actionLabel?: string;
@@ -14,16 +20,47 @@ interface ListingCardProps {
 
 const ListingCard = ({
     data,
-    reservarion,
+    reservation,
     onAction,
     disabled,
     actionLabel,
-    actionId,
+    actionId = '',
     currentUser
 }: ListingCardProps) => {
+    const router = useRouter();
+
+    const { getByValue } = useCountries();
+    const location = getByValue(data.locationValue);
+
+    const handleCancel = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        if (disabled) {
+            return;
+        }
+        onAction?.(actionId);
+    }, [onAction, actionId, disabled]);
+
+    const price = useMemo(() => {
+        if (reservation) {
+            return reservation.totalPrice;
+        }
+        return data.price;
+    }, [reservation, data.price]);
+
+    const reservationDate = useMemo(() => {
+        if (!reservation) {
+            return null;
+        }
+        const startDate = new Date(reservation.startDate);
+        const endDate = new Date(reservation.endDate);
+
+        return `${format(startDate, 'PP')} - ${format(endDate, 'PP')}`
+
+    }, []);
+
     return (
         <div>
-            
+
         </div>
     );
 }
